b26bd09239900a09e14158ca23a6087fc69bd128
Administrator <422517423@qq.com>
2018-09-05 13:42:59 +0800

    添加实体类ext和文件夹，修改域名

---
+++ SpringWind/src/main/java/com/baomidou/springwind/entity/ext/SecondMenuExt.java
@@ -0,0 +1,15 @@
+package com.baomidou.springwind.entity.ext;
+
+import com.baomidou.springwind.entity.SecondMenu;
+
+public class SecondMenuExt extends SecondMenu {
+    private String partName;
+
+    public String getPartName() {
+        return partName;
+    }
+
+    public void setPartName(String partName) {
+        this.partName = partName;
+    }
+}
